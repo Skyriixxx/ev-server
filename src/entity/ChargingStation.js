2311d662b9e359b7daefebcb0e83a24ff3e111e6
Skyrix06 <jason.fabiano@free.fr>
2019-01-30 17:00:22 +0100

    add connector

--- src/entity/ChargingStation.js
+++ src/entity/ChargingStation.js
@@ -8,6 +8,14 @@ class ChargingStation {
         this.chargePointSerialNumber = data.chargePointSerialNumber;
         this.chargePointVendor = data.chargePointVendor;
         this.firmwareVersion = data.firmwareVersion;
+        if (data.hasOwnProperty("connector1")) {
+            this.connector1 = data.connector1;
+        }
+
+        if (data.hasOwnProperty("connector2")) {
+            this.connector2 = data.connector2;
+        }
+
     }
 
     async save() {
